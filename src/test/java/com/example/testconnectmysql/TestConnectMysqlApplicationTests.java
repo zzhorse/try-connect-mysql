00e441fd80684f58811a34e626dfd4e234b7f367
zzhorse <zzhorse@qq.com>
2022-04-28 11:43:28 +0800

    Initial

---
+++ src/test/java/com/example/testconnectmysql/TestConnectMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testconnectmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConnectMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
